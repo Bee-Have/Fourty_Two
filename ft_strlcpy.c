d3be8fa7e6884bc556b0cae7283675363b446a06
Albe MARINI-AUDOUARD <amarini-@e3r11p7.42.fr>
2020-04-28 14:50:15 +0200

    fixed strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -9,7 +9,7 @@ int     ft_strlcpy(char *dst, char *src, int size)
     i = 0;
     if (size < 0)
         return (0);
-    while (src[i] != '\0' && size > 0)
+    while (src[i] != '\0' && size > 1)
     {
         dst[i] = src[i];
         i++;
@@ -21,14 +21,14 @@ int     ft_strlcpy(char *dst, char *src, int size)
 
 int     main(void)
 {
-    char dst[6] = {"Hello "};
-    char src[5] = {"World"};
+    char dst[] = {"Hello"};
+    char src[] = {"World"};
 
-    char dstalt[6] = {"Hello "};
-    char srcalt[5] = {"World"};
+    char dstalt[] = {"Hello"};
+    char srcalt[] = {"World"};
 
-    printf("%s%s , %s%s", dst, src, dstalt, srcalt);
+    printf("%s %s\n%s %s\n\n", dst, src, dstalt, srcalt);
     ft_strlcpy(dst, src, 6);
-    strlcpy(dstalt, srcalt, 5);
-    printf("%s%s , %s%s", dst, src, dstalt, srcalt);
+    strlcpy(dstalt, srcalt, 6);
+    printf("%s %s\n%s %s\n\n", dst, src, dstalt, srcalt);
 }
