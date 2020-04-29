7f91c9ad9a405df1d3fe6f64e5e939cdd285d549
amarini- <duck.marini@gmail.com>
2020-04-29 15:58:45 +0200

    test

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,9 +1,33 @@
-#include <unistd.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
 
 int	ft_isalpha(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
+
+int 	main(void)
+{
+	int i = 0;
+	char test[] = "Hello there general Kenobi";
+
+	printf("%s\n", test);
+	while (test[i] != '\0')
 	{
-
-
+		printf("%d", ft_isalpha(test[i]));
+		i++;
+	}
+	printf("\n");
+	i = 0;
+	while (test[i] != '\0')
+	{
+		printf("%d", isalpha(test[i]));
+		i++;
+	}
 }
