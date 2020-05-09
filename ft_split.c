78da972d6d684e348a2341636226c968b719bf71
Albe MARINI-AUDOUARD <amarini-@e3r2p9.42.fr>
2020-05-09 17:30:35 +0200

    ft_split done i guess ?

--- ft_split.c
+++ ft_split.c
@@ -13,7 +13,6 @@ int		ft_calclen(char const *str, char c, int i)
 		len++;
 		i++;
 	}
-	printf("%d\n", len);
 	return (len);
 }
 
@@ -47,25 +46,18 @@ char	**ft_split(char const *s, char c)
 	il = 0;
 	l = 0;
 	h = ft_calcheig(s, c) + 1;
-	printf("%d, %d, %d\n", i, l, h);
 	res = (char **)malloc(h * (sizeof(char *)));
 	if (!res)
 		return (NULL);
-	printf("first malloc done\n");
 	while (ih < h)
 	{
 		l = ft_calclen(s, c, i);
-		printf("l = %d\nil = %d\n", l, il);
 		res[ih] = (char *)malloc((l + 1) * sizeof(char));
 		if (!res[ih])
 			return(NULL);
-		printf("second malloc done\n");
 		while (il < l)
 		{
-			write(1, "test\n", 5);
-			printf("%d, %c, %c\n", il, res[i][il], s[i]);
 			res[ih][il] = s[i];
-			printf("%c, %c\n", res[ih][il], s[i]);
 			i++;
 			il++;
 		}
@@ -75,13 +67,4 @@ char	**ft_split(char const *s, char c)
 		il = 0;
 	}
 	return (res);
-}
-
-int		main(void)
-{
-	char	test[] = "Hello general";
-	char	**res;
-
-	printf("%s\n", test);
-	res = ft_split(test, ' ');
 }
