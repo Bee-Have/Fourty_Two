a9d2e119b1daabd320377e7e3af90378272bdaea
Albe MARINI-AUDOUARD <amarini-@e1r10p23.42.fr>
2020-09-08 09:58:04 +0200

    ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,43 +6,41 @@
 /*   By: amarini- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/01 09:49:56 by amarini-          #+#    #+#             */
-/*   Updated: 2020/09/07 16:28:22 by amarini-         ###   ########.fr       */
+/*   Updated: 2020/09/08 09:47:30 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
+int		compare(unsigned char chr1, unsigned char chr2)
+{
+	if (chr1 != chr2)
+		return (1);
+	return (0);
+}
+
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned int		i;
-	int					result;
-	char				*copy1;
-	char				*copy2;
+	unsigned int				i;
+	unsigned char				*copy1;
+	unsigned char				*copy2;
 
 	i = 0;
-	result = 0;
 	if (!s1 || !s2)
 		return (0);
-	copy1 = (char *)s1;
-	copy2 = (char *)s2;
-	if(n <= 0)
+	copy1 = (unsigned char *)s1;
+	copy2 = (unsigned char *)s2;
+	if (n <= 0)
 		return (0);
-	while (i <= n && copy1[i] != '\0' && copy2[i] != '\0')
+	while (i < n && copy1[i] != '\0' && copy2[i] != '\0')
 	{
-		printf("%d ", i);
-		if (copy1[i] != copy2[i])
-		{
-			result = copy1[i] - copy2[i];
-			return (result);
-		}
+		if (compare(copy1[i], copy2[i]) == 1)
+			return (copy1[i] - copy2[i]);
 		i++;
 	}
-	if (i <= n)
-		printf("pb from N & I");
-	if (copy1[i] == '\0')
-		printf("pb from copy1 & end 0");
-	if (copy2[i] == '\0')
-		printf("pb from copy2 & end 0");
+	if ((copy1[i] == '\0' || copy2[i] == '\0') &&
+			(compare(copy1[i], copy2[i]) == 1))
+		return (copy1[i] - copy2[i]);
 	return (0);
 }
