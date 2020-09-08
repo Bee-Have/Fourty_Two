30f81dd5c006b38d109e3fbd12d6ef7d66e76b77
Albe MARINI-AUDOUARD <amarini-@e1r10p23.42.fr>
2020-09-08 08:40:33 +0200

    fixed libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amarini- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/07 10:41:19 by amarini-          #+#    #+#             */
-/*   Updated: 2020/09/07 10:55:30 by amarini-         ###   ########.fr       */
+/*   Updated: 2020/09/08 08:39:54 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # define LIBFT_H
 
 void	*ft_memset(void *b, int c, size_t len);
-int		ft_bzero(void *s, siwe_t n);
+int		ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
@@ -36,7 +36,7 @@ int		ft_strlcpy(char *dst, const char *src, size_t dstsize);
 int		ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
-void	*calloc(size_t count, size_t size);
+void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
