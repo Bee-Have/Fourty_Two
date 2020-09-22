ba89e6761cc66b9d57451717e4ce45cdf24245e0
amarini- <amarini-@student.42.fr>
2020-09-22 10:57:00 +0200

    finishing and testing gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amarini- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/21 12:16:18 by amarini-          #+#    #+#             */
-/*   Updated: 2020/09/21 16:54:35 by amarini-         ###   ########.fr       */
+/*   Updated: 2020/09/22 10:37:14 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+
+char	*check_str(char *str)
+{
+	int		i;
+	int		size;
+	char	*result;
+
+	i = 0;
+	size = 0;
+	while (str[size] != '\0')
+	{
+		if (str[size] == '\n')
+		{
+			result = (char *)malloc((size + 1) * sizeof(char));
+			while (i < size)
+			{
+				result[i] = str[i];
+				i++;
+			}
+			result[i] = '\0';
+			return (result);
+		}
+		size++;
+	}
+	return (str);
+}
 
 int		get_next_line(int fd, char **line)
 {
@@ -26,9 +52,9 @@ int		get_next_line(int fd, char **line)
 	if (!buffer)
 		return (-1);
 	current = read(fd, buffer, BUFFER_SIZE);
-	*line[BUFFER_SIZE] = '\0';
-	*line = buffer;
-	//printf("%s\n", *line);
+	buffer[BUFFER_SIZE] = '\0';
+	*line = check_str(buffer);
+	printf("%s\n", *line);
 	if (current < 0)
 		return (-1);
 	if (current > 0)
@@ -39,15 +65,17 @@ int		get_next_line(int fd, char **line)
 int		main(void)
 {
 	int		fd;
-	int		wrt;
+	//int		other;
 	char	*result;
 
 	result = "000000000000";
-	fd = open("foo.txt", O_RDWR);
-	printf("fd=%d\n", fd);
-	wrt = write(fd, "hello there\n", BUFFER_SIZE);
-	printf("result=%d\n", get_next_line(fd, &result));
-	printf("wrote %d number of characters\n", wrt);
-	close(fd);
+	fd = open("/dev/pts/2", O_RDONLY | O_NONBLOCK);
+	if (fd)
+	{
+		printf("fd=%d\n", fd);
+		//other = get_next_line(0, &result);
+		printf("result=%d\n", get_next_line(fd, &result));
+		close(fd);
+	}
 	return (0);
 }
