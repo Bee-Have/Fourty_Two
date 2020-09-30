594a93db344f34341bd64afe2b0f126c1354c7fd
amarini- <amarini-@student.42.fr>
2020-09-30 16:47:04 +0200

    restarting gnl, i get the logic now (hopefully i wont forget it for tomorrow)

--- get_next_line.c
+++ get_next_line.c
@@ -19,20 +19,6 @@
 int		get_next_line(int fd, char **line)
 {
 	static char	*buffer;
-	int			current;
-
-	printf("BUFFER_SIZE=%d\n", BUFFER_SIZE);
-	current = 0;
-	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buffer)
-		return (-1);
-	current = read(fd, buffer, BUFFER_SIZE);
-	buffer[BUFFER_SIZE] = '\0';
-	*line = check_str(buffer);
-	printf("%s\n", *line);
-	if (current < 0)
-		return (-1);
-	if (current > 0)
-		return (1);
-	return (0);
+	
+	
 }
