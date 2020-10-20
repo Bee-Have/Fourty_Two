85c6a8a4620213c5e541af905564ca91caa2b303
amarini- <amarini-@student.42.fr>
2020-10-20 15:43:11 +0200

    working but leaks

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -30,7 +30,7 @@ int		find_newline(char *str, int read)
 	return (-1);
 }
 
-int		read_fd(int fd, int *index, char ***leftover, char **line)
+int		read_fd(int fd, int *index, char **leftover, char **line)
 {
 	int		result;
 	char	*tmp;
@@ -43,70 +43,50 @@ int		read_fd(int fd, int *index, char ***leftover, char **line)
 	{
 		result = read(fd, tmp, BUFFER_SIZE);
 		tmp[result] = '\0';
-		(*leftover)[fd] = ft_strjoin((*leftover)[fd], tmp);
-		*index = find_newline((*leftover)[fd], result);
+		*leftover = ft_strjoin(*leftover, tmp);
+		*index = find_newline(*leftover, result);
 	}
 	free(tmp);
-	if (*index != -1)
-		*line = ft_substr((*leftover)[fd], 0, *index, 1);
+	if (*index == -1)
+		*line = ft_substr(*leftover, 0, ft_strlen(*leftover), 1);
+	else if (*index != -1)
+		*line = ft_substr(*leftover, 0, *index, 1);
 	return (result);
 }
 
-void	free_leftover(char ***leftover)
+void	free_leftover(char **leftover)
 {
-	int		i;
-
-	i = 0;
-	while (i < OPEN_MAX)
-	{
-		free((*leftover)[i]);
-		i++;
-	}
 	free(*leftover);
+	*leftover = NULL;
 }
 
-void	empty_leftover(char ***leftover)
+void	empty_leftover(char **leftover)
 {
-	int		i;
-
-	i = 0;
-	(*leftover)[OPEN_MAX] = NULL;
-	while (i < OPEN_MAX)
-	{
-		(*leftover)[i] = (char *)malloc(1 * sizeof(char));
-		if (!(*leftover)[i])
-		{
-			free_leftover(leftover);
-			return ;
-		}
-		(*leftover)[i][0] = '\0';
-		i++;
-	}
+	*leftover = (char *)malloc(sizeof(char));
+	if (!*leftover)
+		return ;
+	*leftover[0] = '\0';
 }
 
 int		get_next_line(int fd, char **line)
 {
-	static char		**leftover = NULL;
+	static char		*leftover[OPEN_MAX];
 	int				result;
 	int				index;
 
 	result = 1;
-	if (!leftover)
-	{
-		if (!(leftover = (char **)malloc((OPEN_MAX + 1) * sizeof(char *))))
-			return (-1);
-		empty_leftover(&leftover);
-	}
-	index = find_newline(leftover[fd], 0);
+	index = -1;
+	if (!leftover[fd])
+		empty_leftover(&leftover[fd]);
+	else if (leftover[fd][0] != '\0')
+		index = find_newline(leftover[fd], 0);
 	if (index == -1)
-		result = read_fd(fd, &index, &leftover, line);
-	if (index == -1)
-		*line = ft_substr(leftover[fd], 0, ft_strlen(leftover[fd]), 1);
+		result = read_fd(fd, &index, &leftover[fd], line);
 	leftover[fd] = ft_substr(leftover[fd], index + 1,
 		ft_strlen(leftover[fd]) - (index + 1), 0);
 	if (result == -1 || (result == 0 && ft_strlen(leftover[fd]) == 0))
 	{
-		free_leftover(&leftover);
+		free_leftover(&leftover[fd]);
 		return (result);
 	}
 	return (1);
--- main_bonus.c
+++ main_bonus.c
@@ -12,13 +12,16 @@
 
 #include "get_next_line.h"
 
-void	fill_fd(int fd, char *str)
+void	fill_fd(int fd, char *str, int read)
 {
 	int		i;
 
-	i = 1;
-	i = write(fd, str, ft_strlen(str));
-	i = write(fd, "\n", 1);
+	if (read != 0)
+	{
+		i = 1;
+		i = write(fd, str, ft_strlen(str));
+		i = write(fd, "\n", 1);
+	}
 }
 
 int		main(void)
@@ -27,10 +30,18 @@ int		main(void)
 	int		*fd;
 	int		*res;
 	char	*result;
-	int		read;
+	int		read[6];
+	int		end;
 
 	i = 0;
-	read = 1;
+	end = 0;
+	while (i < 5)
+	{
+		read[i] = 1;
+		i++;
+	}
+	read[5] = -5;
+	i = 0;
 	res = (int *)malloc(6 * sizeof(int));
 	fd = (int *)malloc(6 * sizeof(int));
 	if (!fd || !res)
@@ -47,22 +58,33 @@ int		main(void)
 	res[3] = open("result3.txt", O_WRONLY);
 	res[4] = open("result4.txt", O_WRONLY);
 	res[5] = -1;
-	while (read != 0 && read != -1)
+	while (end < 5)
 		{
-		while (fd[i] != -1)
+		while (i < 5)
 		{
-			read = get_next_line(fd[1], &result);
-			fill_fd(res[i], result);
-			printf("line(%d)=[%s]\n", fd[i], result);
-			free(result);
+			if (read[i] > 0)
+			{
+				read[i] = get_next_line(fd[i], &result);
+				fill_fd(res[i], result, read[i]);
+				printf("fd{%d}->[%d]=[%s]\n", fd[i], read[i], result);
+				system ("leaks a.out | grep 'leaked'");
+				free(result);
+				result = NULL;
+			}
+			
+			else if (read[i] == 0)
+			{
+				read[i] = -2;
+				end++;
+			}
+			i++;
 		}
 		i = 0;
 	}
-	fd = 0;
-	if (read == -1)
-		printf("FAIL BITCH\n");
-	while (fd[i] != '\0')
+	i = 0;
+	while (i >= 5)
 	{
+		close(res[i]);
 		close(fd[i]);
 		i++;
 	}
