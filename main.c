6d0b87c10a336ecdcff394eaba13bb91975fad7a
amarini- <amarini-@student.42.fr>
2020-10-21 17:15:05 +0200

    main completely commented

--- main.c
+++ main.c
@@ -23,28 +23,54 @@ void	fill_fd(int fd, char *str)
 
 int		main(void)
 {
+	//le file descriptor qui seras lue 
 	int		fd1;
+	//le file descriptor sur lequel je vais mettre toutes les lignes lues
 	int		fd2;
-	char	*result;
+	//la ligne qui seras envoyer a gnl
+	char	*line;
+	//le retour de gnl:
+	//1=lue
+	//0=fin de lecture
+	//-1=pb pendant gnl
 	int		read;
 
+	//si read = 0 alors rien ne seras lu puisque 0=fin de lecture
+	//read = 1 assure que ce seras bien lue, même une  seule fois
 	read = 1;
-	fd1 = open("foo.txt", O_RDONLY);
+	//ouvre le fichier que l'ont vas lire
+	//(ont ne peut pas lire un fichier que l'ont a pas ouver)
+	//O_RDONLY = on l'ouvre en signifiant que l'ont va uniquement le lire
+	//si l'ont essaie de faire autre chose sur le fichier ont ne pourras pas
+	fd1 = open("text2.txt", O_RDONLY);
+	//ouvre le fichier sur lequel ont vas écrire le résultat
+	//O_WRONLY = ont l'ouvre en signifiant que l'ont vas uniquement écrire
 	fd2 = open("result0.txt", O_WRONLY);
+	//ont vérifie que le fd éxist et qu'il est bien déclarer
 	if (fd1 >= 0 && fd2)
 	{
+		//pendant que le retour de gnl lis
+		//le retour de gnl étant 1, ont lis uniquement quand read=1
 		while (read != 0 && read != -1)
 		{
-			read = get_next_line(fd1, &result);
-			fill_fd(fd2, result);
-			printf("line=[%s]\n", result);
-			//printf("result=%d\n", read);
-			free(result);
+			//appel de gnl, lecture du fichier
+			read = get_next_line(fd1, &line);
+			//fill_fd vas remplir un autre fichier avec les valeurs de retour
+			fill_fd(fd2, line);
+			//print de line, la ligne lue pas gnl
+			printf("line=[%s]\n", line);
+			//ont libère l'espace mémoire de line
+			//ainsi ont ne vas pas assigner de valeurs par dessus celle qui existe
+			//ont auras pas de garbage memory
+			free(line);
 		}
+		//si gnl renvoie -1, c'est une erreure
 		if (read == -1)
 			printf("-1\n");
+		//ont ferme les deux fichier que l'ont a ouvert
 		close(fd1);
 		close(fd2);
 	}
+	//classique return (0) pour signifier la fin du programe
 	return (0);
 }
